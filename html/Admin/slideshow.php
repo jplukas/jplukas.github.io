0d59c5f3548f746c23addcf06201baf1339cc2b6
jplukas <jplukasavicus@gmail.com>
2017-08-02 20:36:17 -0300

    Adicao de imagens na secao do slideshow(admin)

--- html/Admin/slideshow.php
+++ html/Admin/slideshow.php
@@ -7,20 +7,22 @@
     foreach ($slides as $key => $value) {
         echo '<h3>Slide '.$value['id'].'</h3>
                                 <form class="formulario-slideshow" method="POST" action="teste.php">
+
                                     <input type="hidden" value ="'.$value['id'].'" name = "id" /> 
                                     <p>Imagem principal (obrigatório)</p>
                                     <p class="flutuar">Imagem menor</p><br><br>
+                                    <img src="'.$value['imagem_grande'].'" width = "150" height="150">
                                     <input type="file" name="arquivos1" class="btn files"/>
-                                    
+                                    <img src="'.$value['imagem_pequena'].'" width = "150" height="150">
                                     <input type="file" name="arquivos2" class="btn files"/><br><br>
 
                                      <p>Slide Show - Texto maior</p>
                                     <textarea name="texto_grande">'.$value['texto_grande'].'</textarea><br><br>
                                     <p>Slide Show - Texto menor</p>
                                     <textarea name="texto_pequeno">'.$value['texto_pequeno'].'</textarea><br><br>
-                                    <input type="submit">
+                                    <input type="submit" class="btn btn-primary">
                                 </form><br><br>';
     }
 ?>
-<button class="btn ">Adicionar novo slide</button>
+<button class="btn btn-info">Adicionar novo slide</button>
 </div>
